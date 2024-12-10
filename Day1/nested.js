bd26966eac4c0ebe332bb737297a6284158a7e64
saikumar231 <sai20701a04e2@gmail.com>
2024-12-10 15:18:02 +0530

    javascript

---
+++ Day1/nested.js
@@ -0,0 +1,11 @@
+let company = {
+    name: "Tech Corp",
+    employees: [
+      { name: "Alice", position: "Developer" },
+      { name: "Bob", position: "Designer" }
+    ]
+  };
+  
+  console.log(company.employees[0].name);   
+  console.log(company.employees[1].position);  
+  
