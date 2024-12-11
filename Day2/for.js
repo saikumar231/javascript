6620e962c6bddf26b189d3464b1a39a163b253a3
saikumar231 <sai20701a04e2@gmail.com>
2024-12-11 10:09:21 +0530

    javascript

---
+++ Day2/for.js
@@ -0,0 +1,17 @@
+for(var i=0; i<=10; i++){
+    console.log(i);
+}
+
+
+
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
