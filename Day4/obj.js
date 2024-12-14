859a2b56d79b47c242b56a4a054c7ab6d69825b8
saikumar231 <sai20701a04e2@gmail.com>
2024-12-14 10:23:41 +0530

    javascript

---
+++ Day4/obj.js
@@ -0,0 +1,19 @@
+let media={
+    show:"News"
+};
+
+let food={
+    biryani:"Amazing"
+};
+
+let class1={
+    sai:"kumar"
+};
+
+food.__proto__=media;
+class1.__proto__==food;
+
+console.log(food.show);
+console.log(food.biryani);
+console.lof(class1.biryani);
+
